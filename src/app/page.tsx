2280cb02f3e40bb8978d75592e59d6044ba13e81
azevedorafael <rafaelsilva.fatec@gmail.com>
2024-12-16 22:09:22 -0300

    list files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -139,6 +139,7 @@ export default function Home() {
             </label>
             <FileInput.Root>
               <FileInput.Trigger />
+              <FileInput.FileList />
               <FileInput.Control multiple />
             </FileInput.Root>
           </div>
